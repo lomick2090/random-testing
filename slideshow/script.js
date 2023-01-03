881b9005ce6c1c97a389838b7fab9953daee3452
minkman <lomick2090@gmail.com>
2023-01-03 13:04:15 -0600

    Add dot functionality

--- slideshow/script.js
+++ slideshow/script.js
@@ -1,25 +1,47 @@
 let slides = document.querySelector('.slides')
 let slideIndex = 0;
+let dots = document.querySelectorAll('.dot');
 
 function advanceSlide(direction) {
     if (direction == 'right') {
         if (slideIndex != 4) {
-            slideIndex++
+            slideIndex++;
             slides.style.left = `${(slideIndex * -400)}px`;
+            changeChosenDot(slideIndex);
         } else {
             slideIndex = 0;
-            slides.style.left = `0px`
+            slides.style.left = `0px`;
+            changeChosenDot(slideIndex);
         }
     } else if (direction == 'left') {
         if (slideIndex != 0) {
-            slideIndex--
+            slideIndex--;
             slides.style.left = `${(slideIndex * -400)}px`;
+            changeChosenDot(slideIndex);
         } else {
             slideIndex = 4;
-            slides.style.left = `-1600px;`;
+            slides.style.left = `-1600px`;
+            changeChosenDot(slideIndex);
         }
     }
 }
+
+function changeChosenDot (slideNumber) {
+    document.querySelector('.chosen').classList.toggle('chosen');
+    dots[slideNumber].classList.toggle('chosen')
+}
+
+function advanceToSlide(slideNumber) {
+    slideIndex = slideNumber;
+    slides.style.left = `${(slideIndex * -400)}px`;
+}
+
+dots.forEach(dot => { 
+    dot.addEventListener('click', () => {
+    advanceToSlide(dot.id - 1);
+    changeChosenDot(dot.id -1)
+})});
+
 
 document.querySelector('#leftbutton').addEventListener('click', () => advanceSlide('left'));
 document.querySelector('#rightbutton').addEventListener('click', () => advanceSlide('right'))
