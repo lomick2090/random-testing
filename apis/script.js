a1ebec1d7c67307b751d4d0717f722a48825502d
minkman <lomick2090@gmail.com>
2023-01-10 15:40:29 -0600

    Create search functionality

--- apis/script.js
+++ apis/script.js
@@ -1,11 +1,41 @@
 const img = document.querySelector('img');
+const search = document.getElementById('search');
+const weird = document.getElementById('weird');
+let searchValue = search.value;
+let weirdValue = weird.value;
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=t8JRu3bZqBfvr0A1uorQvyFWAkWt1t0B&s=cats', 
-    {mode: 'cors'}
-    )
-    .then((response) => {
-        return response.json();
+function searchGif(search, weird) {
+    let searchURL = `https://api.giphy.com/v1/gifs/translate?api_key=t8JRu3bZqBfvr0A1uorQvyFWAkWt1t0B&s=${search}&weirdness=${weird}`;
+
+    return fetch(searchURL, 
+        {mode: 'cors'}
+        )
+        .then((response) => {
+            return response.json();
+        })
+        .then ((response) => {
+            return response.data.images.original.url
     })
-    .then ((response) => {
-        img.src = response.data.images.original.url
+}
+
+function userSearch() {
+    searchValue = search.value;
+    weirdValue = weird.value;
+
+    searchGif(searchValue, weirdValue)
+        .then((response) => {
+            img.src = response;
+        });
+}
+
+function refreshSearch() {
+    searchGif(searchValue, weirdValue)
+    .then((response) => {
+        img.src = response;
+    });
+}
+
+searchGif('cats', 0)
+    .then((response) => {
+        img.src = response;
     });
